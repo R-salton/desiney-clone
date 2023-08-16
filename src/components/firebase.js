913b0ea8c82ee792d7586e9cf751129be4291f50
R-salton <ringusa22@gmail.com>
2023-08-16 15:30:13 +0200

    New feature

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -10,25 +10,17 @@ import 'firebase/compat/storage';
 // // import { getAnalytics } from "firebase/analytics";
 // import firebase from 'firebase';
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_apiKey,
-//   authDomain: process.env.REACT_APP_authDomain,
-//   projectId: process.env.REACT_APP_projectId,
-//   storageBucket: process.env.REACT_APP_storageBucket,
-//   messagingSenderId: process.env.REACT_APP_messagingSenderId,
-//   appId: process.env.REACT_APP_appId,
-//   measurementId: process.env.REACT_APP_measurementId
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBbX9uoWK6VlMBNzr2Sl0U1L22oZIkM8Q4",
-  authDomain: "desiney-299e3.firebaseapp.com",
-  projectId: "desiney-299e3",
-  storageBucket: "desiney-299e3.appspot.com",
-  messagingSenderId: "352671849845",
-  appId: "1:352671849845:web:806383530140b313eb873c",
-  measurementId: "G-C195TD3PPK"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId
 };
+
+
 
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
